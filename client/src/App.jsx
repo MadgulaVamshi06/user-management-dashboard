956520bf63c3b90c0dfe7b2194d687bb42f599fb
MadgulaVamshi06 <vamshimadgula@gmail.com>
2025-01-21 13:41:39 +0530

    updated user listing table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Create from "./Components/Create";
 import Update from "./Components/Update";
-import Read from "./Components/Read";
-import './App.css';
+import Read from "./Components/UsetDetails";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App d-flex flex-column  align-items-center vh-100">
-  <h1>List Of Users</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/create" element={<Create />}></Route>
-          <Route path="/update/:id" element={<Update />}></Route>
-          <Route path="/read/:id" element={<Read />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/update/:id" element={<Update />}></Route>
+        <Route path="/detail/:id" element={<Read />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
