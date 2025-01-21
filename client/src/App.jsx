fb0c98c4e5e4b6752b5acd1b92cd96c32e17a2de
MadgulaVamshi06 <vamshimadgula@gmail.com>
2025-01-21 11:18:35 +0530

    created table for listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import {BrowserRouter , Routes , Route} from 'react-router-dom'
-import Home from './Pages/Home'
-import Create from './Components/Create'
-import Update from './Components/Update'
-import Read from './Components/Read'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Create from "./Components/Create";
+import Update from "./Components/Update";
+import Read from "./Components/Read";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  
-
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path='/' element={<Home/>}></Route>
-    <Route path='/create' element={<Create/>}></Route>
-    <Route path='/update/:id' element={<Update/>}></Route>
-    <Route path='/read/:id' element={<Read/>}></Route>
-   </Routes>
-   </BrowserRouter>
+    <div className="App d-flex flex-column  align-items-center vh-100">
+  <h1>List Of Users</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/update/:id" element={<Update />}></Route>
+          <Route path="/read/:id" element={<Read />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
